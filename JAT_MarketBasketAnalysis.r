9e6efbf3cfc26e83a42408975e8881a7538c7133
jtorresyap <jtorresyap@users.noreply.github.com>
2016-03-19 17:34:10 -0700

    Update JAT_MarketBasketAnalysis.r

--- JAT_MarketBasketAnalysis.r
+++ JAT_MarketBasketAnalysis.r
@@ -23,7 +23,7 @@ itemFrequencyPlot(Groceries,topN=30, type="absolute")
 # Finding the patterns (aka...rules)
 # Groceries dataset created 410 rules
 # supp =0.00X means that it occured 0.X% in the dataset [LS]
-# conf=0.Y means a confidence level of Y% that it occured when the LS happend
+# conf=0.Y means a confidence level of Y% that it [RS] occured when the [LS] happend
 rules <- apriori(Groceries, parameter = list(supp=0.001, conf=0.8))
 inspect(rules)
 summary(rules)
